ea95d3a9c169f6010f6db76d1ae64aaac96460f4
Numan Arshad <numan.arshad970@gmail.com>
2024-08-22 19:37:00 +0500

    changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,7 @@
 gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
 
-//let links = gsap.utils.toArray("nav a");
-
-let tl = gsap.timeline({
+function initAnimations() {
+  let tl = gsap.timeline({
     scrollTrigger: {
       trigger: ".cards12",
       pin: true,
@@ -13,69 +12,74 @@ let tl = gsap.timeline({
       scrub: 1,
     }
   });
-tl.addLabel("card1");
-tl.to('.card1',{
-  yPercent:0,
-  opacity: 1
-}) 
 
-tl.from('.card2', {
-  yPercent:75,
-  opacity: 0,
-}) 
-tl.addLabel("card2");
-// set the active section based on the direction, and position it part-way through the transition because that's more intuitive
-tl.add(() => setActiveNav(tl.scrollTrigger.direction > 0 ? 1 : 0), "-=0.15"); 
-tl.to('.card1',{
-  scale:0.95,
-  yPercent:-0.5,
-  opacity: 0.7
-}, "-=0.3") 
-
-tl.to('.card2', {
-  yPercent:0,
-  opacity: 1
-}) 
-
-tl.from('.card3', {
-  yPercent:75,
-  opacity: 0,
-}) 
-tl.addLabel("card3");
-tl.add(() => setActiveNav(tl.scrollTrigger.direction > 0 ? 2 : 1), "-=0.15");
-
-tl.to('.card2',{
-  scale:0.98,
-  yPercent:-0.4,
-  opacity: 0.7
-}, "-=0.3") 
-
-tl.to('.card3', {
-  yPercent:0,
-  opacity: 1
-}) 
-
-tl.to('.card3', {});
-
-gsap.utils.toArray(".nav a").forEach((a, i) => {
-  a.addEventListener("click", e => {
-    e.preventDefault();
-    let pad = i === 0 ? 0 : tl.scrollTrigger.direction > 0 ? 2 : -2;
-    gsap.to(window, {scrollTo: labelToScroll(tl, "card" + (i + 1)) + pad})
+  tl.addLabel("card1");
+  tl.to('.card1', {
+    yPercent: 0,
+    opacity: 1
   });
+
+  tl.from('.card2', {
+    yPercent: 75,
+    opacity: 0,
+  });
+  tl.addLabel("card2");
+  tl.add(() => setActiveNav(tl.scrollTrigger.direction > 0 ? 1 : 0), "-=0.15");
+
+  tl.to('.card1', {
+    scale: 0.95,
+    yPercent: -0.5,
+    opacity: 0.7
+  }, "-=0.3");
+
+  tl.to('.card2', {
+    yPercent: 0,
+    opacity: 1
+  });
+
+  tl.from('.card3', {
+    yPercent: 75,
+    opacity: 0,
+  });
+  tl.addLabel("card3");
+  tl.add(() => setActiveNav(tl.scrollTrigger.direction > 0 ? 2 : 1), "-=0.15");
+
+  tl.to('.card2', {
+    scale: 0.98,
+    yPercent: -0.4,
+    opacity: 0.7
+  }, "-=0.3");
+
+  tl.to('.card3', {
+    yPercent: 0,
+    opacity: 1
+  });
+
+  gsap.utils.toArray(".nav a").forEach((a, i) => {
+    a.addEventListener("click", e => {
+      e.preventDefault();
+      let pad = i === 0 ? 0 : tl.scrollTrigger.direction > 0 ? 2 : -2;
+      gsap.to(window, { scrollTo: labelToScroll(tl, "card" + (i + 1)) + pad });
+    });
+  });
+
+  function labelToScroll(timeline, label) {
+    let st = timeline.scrollTrigger,
+        progress = timeline.labels[label] / timeline.duration();
+    return st.start + (st.end - st.start) * progress;
+  }
+
+  let circles = gsap.utils.toArray(".nav .circle");
+
+  function setActiveNav(index) {
+    circles.forEach((circle, i) => circle.classList[i === index ? "add" : "remove"]("active"));
+  }
+}
+
+// Use ScrollTrigger.matchMedia() to apply the animations based on screen size
+ScrollTrigger.matchMedia({
+  "(min-width: 993px)": function() {
+    // Only initialize animations on screens wider than 992px
+    initAnimations();
+  }
 });
-function labelToScroll(timeline, label) {
-  let st = timeline.scrollTrigger,
-      progress = timeline.labels[label] / timeline.duration();
-  return st.start + (st.end - st.start) * progress;
-}
-let circles = gsap.utils.toArray(".nav .circle");
-function setActiveNav(index) {
-  circles.forEach((circle, i) => circle.classList[i === index ? "add" : "remove"]("active"));
-}
-
-
-
-
-
-  
